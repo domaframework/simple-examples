01badab966f6d2d9f48046cbcf69c9d4718d8a50
nakamura-to <toshihiro.nakamura@gmail.com>
2010-01-10 15:32:23 +0000

    繰り返しコメントの仕様変更に対応しました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@550 362887e2-e868-44d1-a061-b03550a0b845

--- doma-jpetstore/src/META-INF/demo/smart/dao/ProductDao/searchProductList.sql
+++ doma-jpetstore/src/META-INF/demo/smart/dao/ProductDao/searchProductList.sql
@@ -6,13 +6,13 @@ SELECT
 FROM 
   PRODUCT
 WHERE
-/*%if keywords.size() > 0 */
-  /*%for keyword : keywords */
+/*%for keyword : keywords */
   lower(name) like /* @contain(keyword.toLowerCase()) */'a' 
   OR 
   lower(category) like /* @contain(keyword.toLowerCase()) */'a' 
   OR 
   lower(descn) like /* @contain(keyword.toLowerCase()) */'a'
-/*%hasNext "  OR"*/
+  /*%if keyword_has_next */
+  OR
   /*%end*/
 /*%end*/

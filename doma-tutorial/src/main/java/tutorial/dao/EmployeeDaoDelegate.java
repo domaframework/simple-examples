86109c21f49ba0505f91fbbbab525dbd849ef5bb
nakamura-to <toshihiro.nakamura@gmail.com>
2010-06-06 12:19:10 +0000

    [DOMA-197][Core] 動的にSQLを組み立てるAPIを提供しました。 1.8.0開発開始。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@875 362887e2-e868-44d1-a061-b03550a0b845

--- doma-tutorial/src/main/java/tutorial/dao/EmployeeDaoDelegate.java
+++ doma-tutorial/src/main/java/tutorial/dao/EmployeeDaoDelegate.java
@@ -15,12 +15,8 @@
  */
 package tutorial.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.builder.SelectBuilder;
 
 public class EmployeeDaoDelegate {
 
@@ -40,29 +36,8 @@ public class EmployeeDaoDelegate {
     }
 
     public int count() {
-        try {
-            Connection connection = config.getDataSource().getConnection();
-            try {
-                PreparedStatement preparedStatement = connection
-                        .prepareStatement("select count(*) from employee");
-                try {
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    try {
-                        if (resultSet.next()) {
-                            return resultSet.getInt(1);
-                        }
-                        return 0;
-                    } finally {
-                        resultSet.close();
-                    }
-                } finally {
-                    preparedStatement.close();
-                }
-            } finally {
-                connection.close();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        SelectBuilder builder = SelectBuilder.newInstance(config);
+        builder.sql("select count(*) from employee");
+        return builder.getSingleResult(int.class);
     }
 }
